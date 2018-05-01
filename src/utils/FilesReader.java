462dde74e3256ca405c9f09a8034f5188a857a82
Mahag <smahagney@gmail.com>
2018-05-01 20:12:43 +0300

    add folder structure

--- src/utils/FilesReader.java
+++ src/utils/FilesReader.java
@@ -1,5 +1,23 @@
 package utils;
 
+import java.io.File;
+
 public class FilesReader {
 
+	public static void main (String args[]) {
+
+		displayIt(new File("C:/Users/Mahag/workspace/TCP-synchronizer/src/resources"));
+	}
+	
+	public static void displayIt(File node){
+		
+		System.out.println(node.getAbsoluteFile());
+		if(node.isDirectory()){
+			String[] subNote = node.list();
+			for(String filename : subNote){
+				displayIt(new File(node, filename));
+			}
+		}
+		
+	}
 }
